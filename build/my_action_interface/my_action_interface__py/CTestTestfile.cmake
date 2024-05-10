2428e2f9f8c8fbf19aa6870fcc2e80d6fee418f2
유용준 <55341412+YongJunYou@users.noreply.github.com>
2024-05-10 13:16:22 +0900

    Delete build directory

--- build/my_action_interface/my_action_interface__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yyj/my_ws/build/my_action_interface/my_action_interface__py
-# Build directory: /home/yyj/my_ws/build/my_action_interface/my_action_interface__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_tf2_package/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/yyj/my_ws/install/my_tf2_package'
